bf0a46cb0438f6bb815730ee370653a71a42a746
Yateen Kedare <yateenkedare95@gmail.com>
2016-10-01 19:43:51 -0400

    Sprint 0 - Iteration 1

---
+++ CarpoolManagementSystem/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Home : Carpool Management</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="NameTextBox" placeholder="What is your name?" runat="server"></asp:TextBox>
+        <asp:Button ID="OKButton" runat="server" Text="Ok" onClick="SubmitNameMethod"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CarpoolManagementSystem/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    MySql.Data.MySqlClient.MySqlConnection mySqlConnection;
+    MySql.Data.MySqlClient.MySqlCommand mySqlCommand;
+    MySql.Data.MySqlClient.MySqlDataReader mySqlDataReader;
+    String queryString,name;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        String connString = "server=localhost;user id=root;password=password;database=test;persistsecurityinfo=True";
+        mySqlConnection = new MySql.Data.MySqlClient.MySqlConnection(connString);
+    }
+
+    protected void SubmitNameMethod(object sender, EventArgs e)
+    {
+        try
+        {
+            mySqlConnection.Open();
+            queryString = "INSERT INTO `test`.`test` (`Name`) VALUES ('" + NameTextBox.Text + "');";
+            mySqlCommand = new MySql.Data.MySqlClient.MySqlCommand(queryString, mySqlConnection);
+            mySqlCommand.ExecuteReader();
+            mySqlConnection.Close();
+            Response.Write("<script>alert('Name Entered successfully');</script>");
+
+
+            mySqlConnection.Open();
+            queryString = "SELECT * FROM `test`.`test` ORDER BY 'Number' DESC LIMIT 1";
+            mySqlCommand = new MySql.Data.MySqlClient.MySqlCommand(queryString, mySqlConnection);
+            mySqlDataReader = mySqlCommand.ExecuteReader();
+            mySqlConnection.Close();
+
+            if (mySqlDataReader.HasRows)
+            {
+                while (mySqlDataReader.Read())
+                {
+                    name = mySqlDataReader.GetString(0);
+                }
+                name = mySqlDataReader.GetString(mySqlDataReader.GetOrdinal("Name"));
+            }
+            
+            Response.Write("<script>alert('Hello "+ name + "');</script>");
+        }
+        catch
+        {
+            Response.Write("<script>alert('Failed to enter in database');</script>");
+        }
+    }
+}
