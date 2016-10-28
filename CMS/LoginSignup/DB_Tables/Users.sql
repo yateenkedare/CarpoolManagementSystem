16c9b253ac3dc185f10f72ae635e36c02d454597
tkonduri <tkonduri@uncc.edu>
2016-10-28 18:58:53 -0400

    Sprint 1 deliverables: 1) Login 2) Signup 3) Search Trips - currently hard coaded data is being displayed, need to fetch it from database 4) Add Trips - Pending

---
+++ CMS/LoginSignup/DB_Tables/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[UserID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [EmaiID] VARCHAR(50) NOT NULL, 
+    [Username] VARCHAR(50) NOT NULL, 
+    [Password] VARCHAR(50) NOT NULL 
+)
