52fd91b759ceba546c2ada399bab2d437964527f
kkashojj-proj <kkashojj@uncc.edu>
2016-10-30 16:27:53 -0400

    Add files via upload

---
+++ CarpoolManagementSystem/CarpoolManagementSystem/CMSTest/AccountControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LoginSignup.Controllers;
+using System.Web.Mvc;
+
+namespace CMSTest
+{
+    /// <summary>
+    /// Summary description for AccountController
+    /// </summary>
+    [TestClass]
+    public class AccountControllerTest
+    {
+
+
+        //public class ResolveAssemblyReference : TaskExtension
+        //{
+
+        //}
+
+        [TestMethod]
+        public void LoginValidation()
+        {
+            AccountController ac = new AccountController();
+            var result = ac.Register() as ViewResult;
+            Assert.AreEqual("Register", result.ViewName);
+        }
+    }
+}
