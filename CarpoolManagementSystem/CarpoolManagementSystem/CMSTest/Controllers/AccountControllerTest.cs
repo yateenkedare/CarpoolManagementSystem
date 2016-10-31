e76d62953cb5e6eee716becdbb8545b2d2fdb1e9
Koushik <kkashojj@uncc.edu>
2016-10-30 20:09:02 -0400

    Arranged tests in folders

---
+++ CarpoolManagementSystem/CarpoolManagementSystem/CMSTest/Controllers/AccountControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LoginSignup.Controllers;
+using System.Web.Mvc;
+
+namespace CMSTest
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void RegisterReturnTest()
+        {
+            var controller = new AccountController();
+            var result = controller.Register() as ViewResult;
+            Assert.AreEqual("Register", result.ViewName);
+        }
+    }   
+}
