7755889b29a720956ee94fa23991e1e57b954ba0
Tejaswi Konduri <tkonduri@uncc.edu>
2016-11-02 07:43:49 -0400

    Removed unnessary temporary files and changes the folder structure a bit..

--- CarpoolManagementSystem/CarpoolManagementSystem/CarpoolManagementSystem/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginSignup.MvcApplication" Language="C#" %>
---
+++ CarpoolManagementSystem/CarpoolManagementSystem/Models/TripModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginSignup.Models
+{
+    public class TripModel
+    {
+        [Required]
+        public string source { get; set; }
+
+        [Required]
+        public string destination { get; set; }
+        
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public /*DateTime*/string date { get; set; }
+
+        //[Required]
+        public string created_by { get { return "ef16bcbc-cc81-4330-a837-5b7f02426c2d"; } }
+
+        [Required]
+        public bool carAvailable { get; set; }
+        
+        public string description { get; set; }
+        
+        public int vacant_seats { get; set; }
+        
+        public float estimated_cost { get; set; }
+    }
+}
