24657eff17fd562f36cb3c1d9888ffac7c9adde1
Yateen Kedare <ykedare@uncc.edu>
2016-12-05 00:08:41 -0500

    added ShowTripDataTest and ShowTripReturnTest

--- CarpoolManagementSystem/CarpoolManagementSystem/CMSTest/Controllers/TripControllerTest.cs
+++ CarpoolManagementSystem/CarpoolManagementSystem/CMSTest/Controllers/TripControllerTest.cs
@@ -12,7 +12,7 @@ namespace CMSTest
     public class TripControllerTest
     {
         [TestMethod]
-        public void TestTripController_TripDetails()
+        public void TripDetailsTest()
         {
             TestTripDbset a = new TestTripDbset();
             Trip a1 = new Trip
@@ -93,38 +93,44 @@ namespace CMSTest
             Assert.AreEqual(trip2, model[0]);
         }
 
-    //[TestMethod]
-    //public void ShowTripReturnTest()
-    //{
-    //    var controller = new TripController();
-    //    var db = new CodeDB();
-    //    var searchbarmodel = new SearchBarModel(){
-    //        source = "charlotte",
-    //        destination = "new york",
-    //        date = "01/01/2026" };
-    //    var result = controller.ShowTripData(searchbarmodel) as ViewResult;
-    //    Assert.AreEqual("ShowTripData", result.ViewName);
-    //}
+        [TestMethod]
+        public void ShowTripReturnTest()
+        {
+            var controller = new TripController();
+            var searchbarmodel = new SearchBarModel()
+            {
+                source = "charlotte",
+                destination = "new york",
+                date = "01/01/2026"
+            };
+            var result = controller.ShowTripData(searchbarmodel) as ViewResult;
+            Assert.AreEqual("ShowTripData", result.ViewName);
+        }
 
-    //[TestMethod]
-    //public void TripdetailsReturnTest()
-    //{
-    //    var controller = new TripController();
-    //    var DB = new CodeDB();
-    //    var result = controller.TripDetails(7) as ViewResult;
-    //    Assert.AreEqual("TripDetails", result.ViewName);
-    //}
+        [TestMethod]
+        public void TripdetailsReturnTest()
+        {
+            TestTripDbset a = new TestTripDbset();
+            Trip a1 = new Trip
+            {
+                id = 1,
+                source = "durham",
+                destination = "hyderabad",
+                created_by = "bhanu",
+                carAvailable = true,
+                vacant_seats = 4,
+                estimated_cost = 25,
+                description = null
+            };
+            a.Add(a1);
+            TestDbContextTrip t_c_t = new TestDbContextTrip(a);
+            t_c_t.Trips.Add(a1);
+            var controller = new TripController(t_c_t);
+            var result = controller.TripDetails(1) as ViewResult;
+            Assert.AreEqual("TripDetails", result.ViewName);
+        }
 
-    //[TestMethod]
-    //public void ShowTripTest()
-    //{
-    //    var controller = new TripController();
-    //    CodeDB DB = new CodeDB();
-    //    var result = controller.ShowJoinButton("7");
-    //    Assert.AreEqual(true, result);
-    //}
-
-    [TestMethod]
+        [TestMethod]
         public void ShowJoinButtonTest()
         {
             TestTripDbset a = new TestTripDbset();
